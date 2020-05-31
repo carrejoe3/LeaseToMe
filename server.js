51d6c5e4e9b504c62dbd6f60eb2f7a764d6a4cc7
Joe <carre.joe3@gmail.com>
2020-05-31 15:24:04 +0100

    installed morgan dependency

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('combined'))
 
 // const Users = require('./routes/Users')
 
