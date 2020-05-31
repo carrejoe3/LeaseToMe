78c7d27cc8d93bea68b20b0c8a23185894577228
Joe <carre.joe3@gmail.com>
2020-05-31 14:54:56 +0100

    installed eslint for backend

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('databaseName', 'root', '', {
+const sequelize = new Sequelize('databaseName', 'root', 'password', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false,
