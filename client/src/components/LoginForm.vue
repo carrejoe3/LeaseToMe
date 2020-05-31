dbaab765362eef77796122de26311d8a7ce8dc6d
Joe <carre.joe3@gmail.com>
2020-05-31 18:40:26 +0100

    moved login form into its own component, and passed login credentials to api call

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar color="primary" dark flat>
+      <v-toolbar-title>Login form</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username" name="username" prepend-icon="mdi-account" type="text" v-model="username"></v-text-field>
+        <v-text-field id="password" label="Password" name="password" prepend-icon="mdi-lock" type="password" v-model="password"></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" @click="login">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+import authentication from '@/services/authenication.js'
+
+export default {
+  name: 'LoginForm',
+  data: () => ({
+    username: null,
+    password: null
+  }),
+  methods: {
+    login () {
+      authentication.login({
+        username: this.username,
+        password: this.password
+      })
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
