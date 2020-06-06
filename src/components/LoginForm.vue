581b8b3501539cef9cb69a4ac50e803656eed478
Joe <carre.joe3@gmail.com>
2020-06-06 17:12:54 +0100

    users can now register and login using the prebuilt amplify components

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,24 +1,10 @@
 <template>
-  <v-card class="elevation-12">
-    <v-toolbar color="primary" dark flat>
-      <v-toolbar-title>Login form</v-toolbar-title>
-    </v-toolbar>
-    <v-card-text>
-      <v-form>
-        <v-text-field label="Username" name="username" prepend-icon="mdi-account" type="text" v-model="username"></v-text-field>
-        <v-text-field id="password" label="Password" name="password" prepend-icon="mdi-lock" type="password" v-model="password"></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="primary" @click="login">Login</v-btn>
-    </v-card-actions>
-  </v-card>
+  <amplify-authenticator>
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
 </template>
 
 <script>
-
-import authentication from '@/services/authenication.js'
 
 export default {
   name: 'LoginForm',
@@ -26,16 +12,8 @@ export default {
     username: null,
     password: null
   }),
-  methods: {
-    async login () {
-      const response = await authentication.login({
-        username: this.username,
-        password: this.password
-      })
-
-      console.log(response)
-    }
-  }
+  methods: {},
+  mounted () {}
 }
 
 </script>
