7ed17fdb691ec3e29de28b2ed4f8b8c2b46accae
Joe <carre.joe3@gmail.com>
2020-06-06 15:44:01 +0100

    installed additional aws dependencies

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import awsExports from './aws-exports'
+
+Amplify.configure(awsExports)
 
 Vue.config.productionTip = false
 
