6ba618c8c0f49b9712373ba3e405bef356fe3bb8
Joe <carre.joe3@gmail.com>
2020-06-21 20:53:41 +0100

    Update HeroImage.vue

--- src/components/home/HeroImage.vue
+++ src/components/home/HeroImage.vue
@@ -3,8 +3,15 @@
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
         <h1 class="text-h3 font-weight-bold mb-4">The Better Way to Lease NYC Retail Space</h1>
-        <h4 class="text-h5">Streamline your search</h4>
+        <h4 class="text-h5">Streamline your search.</h4>
+        <h4 class="text-h5 mt-16">Submit your commercial real estate needs and let landlords compete for you.</h4>
       </v-col>
     </v-row>
   </v-parallax>
 </template>
+
+<style lang="scss" scoped>
+h1, h4 {
+  text-shadow: 1px 1px 1px #000;
+}
+</style>
