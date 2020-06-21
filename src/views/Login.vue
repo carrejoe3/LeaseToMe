2106fab92e2d7b4cc616e97c1833ab199f587093
Joe <carre.joe3@gmail.com>
2020-06-21 18:42:20 +0100

    added the main navbar to the login screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,26 @@
 <template>
-  <v-container class="fill-height" fluid>
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
-        <LoginForm />
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-main>
+    <MainNavbar />
+    <v-container fluid id="login">
+      <v-row align="center" justify="center" class="fill-height">
+        <v-col cols="12" sm="8" md="4">
+          <LoginForm />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
 
+import MainNavbar from '@/components/navbars/MainNavbar'
 import LoginForm from '@/components/login/LoginForm.vue'
 
 export default {
   name: 'Login',
   components: {
-    LoginForm
+    LoginForm,
+    MainNavbar
   },
   mounted () {
     this.$store.commit('setState', { property: 'currentPage', value: 'login' })
@@ -25,5 +30,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+#login {
+  height: calc(100vh - 94px);
+}
 </style>
