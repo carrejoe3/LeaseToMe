5795c29cb17521ed2e441051372dd5e30eec3a33
Joe <carre.joe3@gmail.com>
2020-06-25 11:54:49 +0100

    continued work on the first page of property listing questions

--- src/components/propertyListing/locationQs.vue
+++ src/components/propertyListing/locationQs.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-card color="lightGrey" width="800" class="mx-auto ma-5">
-    <v-card-title class="submissionTitle pt-10 text-center">Where is your property located?</v-card-title>
+  <v-card color="lightGrey" width="800" class="mx-auto ma-5" id="locationQs">
+    <v-card-title class="submissionTitle pt-10">Where is your property located?</v-card-title>
     <v-card-text>
-      <v-btn @click="selectBorough(btn.value)" v-for="(btn, index) in locationBtns" :key="index" class="ma-5" :color="btn.active ? 'primary' : 'lightGrey'">{{ btn.title }}</v-btn>
-      <v-text-field class="ma-5" solo></v-text-field>
-      <v-text-field class="ma-5" solo></v-text-field>
+      <v-btn @click="selectBorough(btn.value)" v-for="(btn, index) in locationBtns" :key="index" class="ma-4" :color="btn.active ? 'primary' : 'lightGrey'">{{ btn.title }}</v-btn>
+      <div class="d-flex align-center ma-5" v-if="property.selectedBorough !== null">
+        <div class="text-h6 qLabel mr-5">Neighborhood</div>
+        <v-text-field solo hide-details v-model="property.neighborhood"></v-text-field>
+      </div>
+      <div class="d-flex align-center ma-5" v-if="property.selectedBorough !== null">
+        <div class="text-h6 qLabel mr-5">Address</div>
+        <v-text-field solo hide-details v-model="property.address"></v-text-field>
+      </div>
     </v-card-text>
+    <v-card-actions class="justify-space-around">
+      <v-btn x-large color="primary" class="mb-5" :disabled="!nextBtnEnabled">
+        Next
+        <v-icon>mdi-arrow-right</v-icon>
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -40,8 +52,7 @@ export default {
           value: 'statenIsland',
           active: false
         }
-      ],
-      selectedBorough: null
+      ]
     }
   },
   computed: {
@@ -53,18 +64,21 @@ export default {
         this.$store.commit('setState', { property: 'propertyListingQsStep', value: value })
       }
     },
-    propertyListingData: {
+    property: {
       get () {
         return this.$store.state.propertyListingData
       },
       set (value) {
-        this.$store.commit('setState', { property: 'propertyListingData', value: value })
+        this.$store.commit('setState', { property: 'property', value: value })
       }
+    },
+    nextBtnEnabled () {
+      return this.property.address !== '' && this.property.neighborhood !== '' && this.property.address !== null && this.property.neighborhood !== null && this.property.selectedBorough !== null
     }
   },
   methods: {
     selectBorough (borough) {
-      this.propertyListingData.selectedBorough = borough
+      this.property.selectedBorough = borough
 
       for (const i in this.locationBtns) {
         this.locationBtns[i].active = false
@@ -75,3 +89,13 @@ export default {
 }
 
 </script>
+
+<style lang="scss" scoped>
+#locationQs {
+  text-align: center;
+
+  .qLabel {
+    color: $primary;
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,9 @@ export default new Vuex.Store({
     currentPage: '',
     propertyListingQsStep: 1,
     propertyListingData: {
-      selectedBorough: null
+      selectedBorough: null,
+      neighborhood: null,
+      address: null
     }
   },
   mutations: {
