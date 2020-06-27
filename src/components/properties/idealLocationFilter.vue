20fc9d0f2151b4aeb1329b8401b7a86ee80c745f
Joe <carre.joe3@gmail.com>
2020-06-27 19:24:09 +0100

    Update idealLocationFilter.vue

--- src/components/properties/idealLocationFilter.vue
+++ src/components/properties/idealLocationFilter.vue
@@ -2,6 +2,7 @@
   <v-container id="idealLocationFilter" fluid>
     <v-container>
       <div id="filterTitle" class="mt-3 text-h5">What's the ideal location or your business?</div>
+      <v-btn @click="selectLocation(btn)" class="ma-2 mt-5" v-for="(btn, index) in filterOptions" :key="index" :color="btn.active ? 'primary' : ''">{{ btn.value }}</v-btn>
     </v-container>
   </v-container>
 </template>
@@ -10,6 +11,36 @@
 
 export default {
   name: 'idealLocationFilter',
+  data () {
+    return {
+      filterOptions: [
+        {
+          value: 'Brooklyn',
+          active: false
+        },
+        {
+          value: 'Manhatten',
+          active: false
+        },
+        {
+          value: 'Staten Island',
+          active: false
+        },
+        {
+          value: 'Queens',
+          active: false
+        },
+        {
+          value: 'Bronx',
+          active: false
+        },
+        {
+          value: 'All',
+          active: true
+        }
+      ]
+    }
+  },
   computed: {
     idealLocationFilter: {
       get () {
@@ -17,6 +48,16 @@ export default {
       },
       set (value) {
         this.$store.commit('setState', { property: 'idealLocationFilter', value: value })
+      }
+    }
+  },
+  methods: {
+    selectLocation (filter) {
+      this.idealLocationFilter = filter.value
+
+      for (const i in this.filterOptions) {
+        this.filterOptions[i].active = false
+        if (this.filterOptions[i].value === filter.value) this.filterOptions[i].active = true
       }
     }
   }
